9dc41e766cd40449951dd9c523f3f6539e6519fb
Michael Johns <michael.johns@apiture.com>
2021-11-03 12:23:36 -0600

    Make it easier to add a breakpoint.

--- apps/tester/src/tests/veryLongTest.spec.js
+++ apps/tester/src/tests/veryLongTest.spec.js
@@ -1,6 +1,8 @@
 describe('tests with specific status', () => {
   test('should pass after 5 seconds', (done) => {
     // if this is set to 5000, the first suite has time to finish reporting
-    setTimeout(() => done(), 0);
+    setTimeout(() => {
+      done();
+    }, 0);
   });
 });

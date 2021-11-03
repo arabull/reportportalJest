52bbcf0dd681fa06c49f20cf2d1f9a019ce3fc22
Michael Johns <michael.johns@apiture.com>
2021-11-03 12:14:35 -0600

    Make sure tests are run alphabetically.

--- apps/tester/jest.config.js
+++ apps/tester/jest.config.js
@@ -1,18 +1,6 @@
 module.exports = {
   testRunner: 'jest-circus/runner',
   testRegex: ['./tests/.*.spec.js?$'],
-  reporters: [
-    'default',
-    [
-      '@reportportal/agent-js-jest',
-      {
-        token: 'xxx',
-        endpoint: 'https://reportportal.apiture.com/api/v1',
-        launch: 'TINY_REPO_LAUNCH_TESTING',
-        project: 'default_personal',
-        description: 'Testing RP with NX and Jest',
-        debug: true,
-      },
-    ],
-  ],
+  testSequencer: './src/tests/sequencer.js',
+  reporters: ['./src/reporters/custom-reporter'],
 };

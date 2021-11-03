b61c33ee39da40697e47c71979b64dca76f55ec5
Michael Johns <michael.johns@apiture.com>
2021-11-03 14:41:56 -0600

    2 seconds is enough.

--- apps/tester/src/tests/veryLongTest.spec.js
+++ apps/tester/src/tests/veryLongTest.spec.js
@@ -1,6 +1,6 @@
 describe('tests with specific status', () => {
-  test('should pass after 5 seconds', (done) => {
-    // if this is set to 5000, the first suite has time to finish reporting
+  test('should pass after 2 seconds', (done) => {
+    // if this is set to 2000, the first suite has time to finish reporting
     setTimeout(() => {
       done();
     }, 0);
